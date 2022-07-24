bc5e1e0e88039393bae3ba3ac4687b39e3f7b9a5
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-07-24 12:46:18 +0800

    Revert "Merge pull request #337 from misuzu/additive-features"
    This reverts commit 726748de61bc7561e76e9a8c7b72b929aed33c30, reversing
    changes made to 876912d59ef5850177f77af783aa699ac4f111cc.

--- src/config.rs
+++ src/config.rs
@@ -203,7 +203,7 @@ impl Config {
     }
     /// Set TLS Configuration (Native TLS)
     #[cfg_attr(feature = "docs", doc(cfg(feature = "h1-client")))]
-    #[cfg(all(feature = "h1-client", not(feature = "h1-client-rustls")))]
+    #[cfg(feature = "h1-client")]
     pub fn set_tls_config(
         mut self,
         tls_config: Option<std::sync::Arc<async_native_tls::TlsConnector>>,
