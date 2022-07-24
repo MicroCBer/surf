19520b2de1e7c058f61a07ae27ccdd7a9a162ee8
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-07-24 13:11:47 +0800

    Update mod.rs

--- src/middleware/redirect/mod.rs
+++ src/middleware/redirect/mod.rs
@@ -107,18 +107,18 @@ impl Middleware for Redirect {
                     println!("2");
                     let http_req: &mut http::Request = req.as_mut();
                     println!("3 {:#?}", Url::parse(location.last().as_str()));
-                    //             *http_req.url_mut() = match Url::parse(location.last().as_str()) {
-                    //                 Ok(valid_url) => {
-                    //                     base_url = valid_url;
-                    //                     base_url.clone()
-                    //                 }
-                    //                 Err(e) => match e {
-                    //                     http::url::ParseError::RelativeUrlWithoutBase => {
-                    //                         base_url.join(location.last().as_str())?
-                    //                     }
-                    //                     e => return Err(e.into()),
-                    //                 },
-                    //             };
+                                *http_req.url_mut() = match Url::parse(location.last().as_str()) {
+                                    Ok(valid_url) => {
+                                        base_url = valid_url;
+                                        base_url.clone()
+                                    }
+                                    Err(e) => match e {
+                                        http::url::ParseError::RelativeUrlWithoutBase => {
+                                            base_url.join(location.last().as_str())?
+                                        }
+                                        e => return Err(e.into()),
+                                    },
+                                };
                 }
             } else {
                 println!("4");
